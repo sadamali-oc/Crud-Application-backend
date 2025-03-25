4bb5a3a783a8c3ff9bcdf7c50b435bee046a80a5
Chamalka Obadage <sadamalioc.21@uom.lk>
2025-03-25 12:50:44 +0530

    Establish the port for db

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const server = express;
+const server = express();
+server.use(bodyParser.json());
 
 
 //Establishing connection to the database
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'dbschool'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "dbschool",
 });
 
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
+db.connect(function (error) {
+  if (error) {
+    console.log("Error Connecting to DB");
+  } else {
+    console.log("successfully Connected to DB");
+  }
 });
 
+//Establish the Port
 
-
-
-
+server.listen(3000, function check(error) {
+  if (error) {
+    console.log("Error....dddd!!!!");
+  } else {
+    console.log("Server Started on Port 3000");
+  }
+});
